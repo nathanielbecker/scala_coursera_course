76b811c76376cf0a460050b74b4ddc08a1101504
Nathaniel Becker <nbecker@nbecker-mn.linkedin.biz>
2013-10-02 14:22:23 -0700

    removing some stuff

--- scala_coursera_class/workspace/hello_world/src/greeter/fffff.scala
+++
@@ -1,9 +0,0 @@
-package greeter
-
-object fffff {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
-  val x = 1                                       //> x  : Int = 1
-  def increase(i: Int) = i + 1                    //> increase: (i: Int)Int
-  increase(x)                                     //> res0: Int = 2
-
-}
